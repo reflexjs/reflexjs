60da84066db689ffd9732bcb1a91438458d131b8
arshad <arshad@chummun.com>
2020-07-02 18:41:39 +0400

    fix: update packaging

--- packages/gatsby-theme-base/gatsby-config.js
+++ packages/gatsby-theme-base/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = () => {
   return {
     plugins: [
+      `@reflexjs/gatsby-plugin-metatags`,
       `@reflexjs/gatsby-plugin-image`,
       `@reflexjs/gatsby-theme-core`,
       `@reflexjs/gatsby-theme-block`,
