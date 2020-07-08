7fd586eaa49982d3d23b603a6aafe02a56bc90fb
arshad <arshad@chummun.com>
2020-07-08 16:10:43 +0400

    feat(starters): update the base starter

--- starters/gatsby-starter-base/gatsby-config.js
+++ starters/gatsby-starter-base/gatsby-config.js
@@ -3,7 +3,7 @@ require(`dotenv`).config()
 module.exports = {
   siteMetadata: {
     title: "Reflex",
-    description: "Starter for the Reflex.",
+    description: "Starter for Reflex.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
   plugins: ["@reflexjs/gatsby-theme-base"],
