be10e11f161daeeff909afc80c670cc5740eba4d
arshad <arshad@chummun.com>
2020-07-28 22:50:27 +0400

    test: add gatsby-theme-video to e2e-tests-blocks

--- e2e-tests/blocks/gatsby-config.js
+++ e2e-tests/blocks/gatsby-config.js
@@ -4,5 +4,9 @@ module.exports = {
     description: "Reflex site",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
-  plugins: [`@reflexjs/gatsby-theme-base`, `@reflexjs/gatsby-theme-post`],
+  plugins: [
+    `@reflexjs/gatsby-theme-base`,
+    `@reflexjs/gatsby-theme-post`,
+    `@reflexjs/gatsby-theme-video`,
+  ],
 }
