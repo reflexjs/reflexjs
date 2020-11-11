c0ca6db1b90853c4cca26adb8e8fb60633ca5393
arshad <arshad@chummun.com>
2020-11-11 19:23:32 +0400

    feat(reflexjs): add cli command to create theme

--- packages/reflexjs/src/cli.ts
+++ packages/reflexjs/src/cli.ts
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 "use strict"
 
-console.log("🎉 Created theme file: theme.js")
+import { resolve } from "path"
+import { readFileSync, writeFileSync } from "fs"
+const { preset = "default" } = require("minimist")(process.argv.slice(2))
+
+const themeStub = readFileSync(
+  resolve(__dirname, `../stubs/${preset}.theme.js`),
+  "utf-8"
+)
+
+writeFileSync("./theme.js", themeStub)
+
+console.log(`🎉 Created a new theme.js file with the ${preset} preset.`)
---
+++ packages/reflexjs/stubs/default.theme.js
@@ -0,0 +1,40 @@
+module.exports = {
+  breakpoints: ["640px", "768px", "1024px", "1280px"],
+  colors: {
+    text: "#111",
+    background: "#fff",
+    primary: "#06f",
+    modes: {
+      dark: {
+        text: "#ededee",
+        background: "#1a202c",
+        primary: "#fb3640",
+      },
+    },
+  },
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: "system-ui, sans-serif",
+    monospace: "Menlo, monospace",
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  styles: {
+    global: {
+      "*": {
+        m: 0,
+        p: 0,
+        listStyle: "none",
+        border: "0 solid",
+        borderColor: "border",
+      },
+    },
+  },
+}
