2e569b3a41991070b091e19f83dd273671dc4663
arshad <arshad@chummun.com>
2020-11-23 21:17:15 +0400

    fix(codemod): update bin

---
+++ packages/codemod/bin/reflexjs-codemod.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+// Based on https://github.com/reactjs/react-codemod/blob/dd8671c9a470a2c342b221ec903c574cf31e9f57/bin/react-codemod.js
+
+require('./cli').run()
