bb44c914bab5b637462ecf87c8d2d92c107a6db2
LocTran016 <trungphuc016@gmail.com>
2020-11-24 17:03:08 +0700

    Update present.js

--- packages/gatsby-theme-present/src/present.js
+++ packages/gatsby-theme-present/src/present.js
@@ -20,16 +20,16 @@ export const Present = ({
   <link rel="stylesheet" href={"https://cdn.jsdelivr.net/npm/reveal.js@latest/dist/theme/" + {slideTheme} + ".css"} />
   <link rel="stylesheet" href={"https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/highlight/"+ {slideCodeTheme} + ".css"} />
   </Helmet>
-  <Div className="reveal"><Div class="slides">
+  <Div className="reveal"><Div className="slides">
       <MDXRenderer><Section data-markdown><Textarea data-template>{body}</Textarea></Section></MDXRenderer>
-  </Div></Div>
+  </Div></Div> {/*Slide contents go here*/}
   <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/dist/reveal.js"></Script>
   <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/zoom/zoom.js"></Script>
   <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/notes/notes.js"></Script>
   <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/math/math.js"></Script>
   <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/markdown/markdown.js"></Script>
   <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/highlight/highlight.js"></Script>
-  {slidePlugin}
+  {slidePlugin} {/*Adding some reveal.js plugins*/}
   {/* <Script
   dangerouslySetInnerHTML={{
     __html: `
@@ -75,3 +75,27 @@ export const Present = ({
 export const revealConfig = ({ pageContext }) => {
   __html: {pageContext.themeOptions.slide.config}
 }
+{/* Reveal.js Configs, may look like this
+    Reveal.initialize({
+      mouseWheel: true,
+      autoSlide: true,
+      loop: false,
+      progress: false,
+      showNotes: true,
+      autoPlayMedia: true,
+      markdown: {
+        smartypants: true
+      },
+      plugins: [
+      RevealMarkdown,
+      // Markdown
+      RevealHighlight
+      // Search
+      RevealSearch,
+      // Speaker notes
+      RevealNotes,
+      // Zoom in and out with Alt+click
+      RevealZoom,
+      // MathJax
+      RevealMath
+      ]})*/}
