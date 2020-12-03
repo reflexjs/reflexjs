83f821e69e18e097a8d0dff9e408873d4b80d673
arshad <arshad@chummun.com>
2020-12-03 10:03:05 +0400

    fix(core): update button link with missing variant

--- packages/gatsby-theme-core/src/button-link.js
+++ packages/gatsby-theme-core/src/button-link.js
@@ -1,12 +1,11 @@
 /** @jsx jsx */
 import { jsx } from "reflexjs"
 
-export const ButtonLink = ({ href, to, variant, children, ...props }) => (
-  <a
-    variant={`button.${variant.split(" ").join(".")}`}
-    href={href || to}
-    {...props}
-  >
-    {children}
-  </a>
-)
+export const ButtonLink = ({ href, to, variant, children, ...props }) => {
+  const variants = variant ? `.${variant.split(" ").join(".")}` : null
+  return (
+    <a variant={`button${variants}`} href={href || to} {...props}>
+      {children}
+    </a>
+  )
+}
