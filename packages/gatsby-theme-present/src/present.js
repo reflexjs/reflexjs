7bfa55f85e4c14536eb34d0b9c69cd6ea7aed3c1
LocTran016 <trungphuc016@gmail.com>
2020-12-06 19:38:36 +0700

    Fix errors, change into npm

--- packages/gatsby-theme-present/src/present.js
+++ packages/gatsby-theme-present/src/present.js
@@ -1,101 +1,39 @@
 import * as React from "react"
 import { MDXRenderer } from "@reflexjs/gatsby-theme-core"
-import { Div, Script } from "@reflexjs/components"
-import { Helmet } from "react-helmet"
+import { Div } from "@reflexjs/components"
+import Reveal from 'reveal.js'
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
+import Zoom from 'reveal.js/plugin/zoom/zoom.esm.js'
+import Notes from 'reveal.js/plugin/notes/notes.js'
+import Math from 'reveal.js/plugin/math/math.esm.js'
+import Hightlight from 'reveal.js/plugin/highlight/highlight.esm.js'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
 
 export const Present = ({
   body,
   pageContext,
 }) => {
-  const slideCodeTheme = defaultSlide.codeTheme || 'monokai'
-  const defaultSlide = pageContext.themeOptions.slide
-  // const slide = theme.slide Used for options in frontmatters
-  const slideTheme = defaultSlide.theme || "black"
-  const slidePlugin = slideTheme.plugin
+  const slideOptions = pageContext.themeOptions.slide
+  const slideConfig = slideOptions.config
+  const separator = slideOptions.separator || "---"
+  const verticalSeparator = slideOptions.verticalSeparator || "--"
+  React.useEffect(() => {
+    let present = new Reveal({
+      slideConfig,
+      plugins: [ 
+        Markdown,
+        Notes,
+        Zoom,
+        Math,
+        Hightlight,
+       ]
+     })
+     present.initialize();
+},[])
    return(
-  <Div>
-  <Helmet>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js@latest/dist/reveal.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js@latest/dist/reset.css" />
-  <link rel="stylesheet" href={"https://cdn.jsdelivr.net/npm/reveal.js@latest/dist/theme/" + {slideTheme} + ".css"} />
-  <link rel="stylesheet" href={"https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/highlight/"+ {slideCodeTheme} + ".css"} />
-  </Helmet>
-  <Div className="reveal"><Div className="slides">
-      <MDXRenderer><Section data-markdown><Textarea data-template>{body}</Textarea></Section></MDXRenderer>
-  </Div></Div> {/*Slide contents go here*/}
-  <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/dist/reveal.js"></Script>
-  <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/zoom/zoom.js"></Script>
-  <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/notes/notes.js"></Script>
-  <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/math/math.js"></Script>
-  <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/markdown/markdown.js"></Script>
-  <Script src="https://cdn.jsdelivr.net/npm/reveal.js@latest/plugin/highlight/highlight.js"></Script>
-  {slidePlugin} {/*Adding some reveal.js plugins*/}
-  {/* <Script
-  dangerouslySetInnerHTML={{
-    __html: `
-    Reveal.initialize({
-      mouseWheel: {mouseWheel},
-      transition: '!{transition}',
-      transitionSpeed: '!{transitionSpeed}',
-      parallaxBackgroundImage: '!{parallaxBackgroundImage}',
-      parallaxBackgroundSize: '!{parallaxBackgroundSize}',
-      parallaxBackgroundHorizontal: '!{parallaxBackgroundHorizontal}',
-      parallaxBackgroundVertical: '!{parallaxBackgroundVertical}',
-      autoSlide: !{autoSlide},
-      loop: !{loop},
-      controlsLayout: '!{controlsLayout}', // Determines where controls appear, "edges" or "bottom-right"
-      controlsBackArrows: '!{controlsBackArrows}',
-      progress: !{progress},
-      showNotes: !{showNotes},
-      autoPlayMedia: !{autoPlayMedia},
-      backgroundTransition: '!{backgroundTransition}',
-      markdown: {
-        smartypants: true
-      &rbrace;,
-      plugins: [
-      RevealMarkdown,
-      // Markdown
-      RevealHighlight
-      // Search
-      RevealSearch,
-      // Speaker notes
-      RevealNotes,
-      // Zoom in and out with Alt+click
-      RevealZoom,
-      // MathJax
-      RevealMath
-      ]})
-        `,
-  }}/> */}
-  <Script
-  dangerouslySetInnerHTML={revealConfig()} />
+  <Div className="slides" data-separator={separator} data-separator-vertical={verticalSeparator}>
+    <MDXRenderer><Section data-markdown><Textarea data-template>{body}</Textarea></Section></MDXRenderer>
   </Div>
 )}
 
-export const revealConfig = ({ pageContext }) => {
-  __html: {pageContext.themeOptions.slide.config}
-}
-{/* Reveal.js Configs, may look like this
-    Reveal.initialize({
-      mouseWheel: true,
-      autoSlide: true,
-      loop: false,
-      progress: false,
-      showNotes: true,
-      autoPlayMedia: true,
-      markdown: {
-        smartypants: true
-      },
-      plugins: [
-      RevealMarkdown,
-      // Markdown
-      RevealHighlight
-      // Search
-      RevealSearch,
-      // Speaker notes
-      RevealNotes,
-      // Zoom in and out with Alt+click
-      RevealZoom,
-      // MathJax
-      RevealMath
-      ]})*/}
