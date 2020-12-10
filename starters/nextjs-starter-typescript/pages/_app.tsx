50060bb9ef44d05eab002f97e2ceeb5b1e7aea78
arshad <arshad@chummun.com>
2020-12-10 16:50:22 +0400

    fix: update nextjs-starter-typescript

--- starters/nextjs-starter-typescript/pages/_app.tsx
+++ starters/nextjs-starter-typescript/pages/_app.tsx
@@ -1,13 +1,12 @@
-import { AppProps } from "next/dist/next-server/lib/router/router"
+import * as React from "react"
+import { AppProps } from "next/app"
 import { ThemeProvider } from "reflexjs"
 import theme from "../src/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
