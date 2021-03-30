70d5f0c0a15bc6e5bcb9675e09aa2da1e751757f
arshad <arshad@chummun.com>
2021-03-30 12:03:20 +0400

    fix(remark-mdx-searchable): update tests fixtures

--- packages/remark-mdx-searchable/test/test.js
+++ packages/remark-mdx-searchable/test/test.js
@@ -2,17 +2,12 @@ const fs = require("fs")
 const path = require("path")
 const remark = require("remark")
 const mdx = require("remark-mdx")
-const frontmatter = require("remark-frontmatter")
 const searchable = require("..")
 
 const doc = fs.readFileSync(path.join(__dirname, "./fixture.mdx"), "utf8")
 
 it("extracts searchable sections from mdx", () => {
-  const result = remark()
-    .use(frontmatter)
-    .use(mdx)
-    .use(searchable)
-    .processSync(doc)
+  const result = remark().use(mdx).use(searchable).processSync(doc)
 
   expect(result.data).toMatchSnapshot()
 })
